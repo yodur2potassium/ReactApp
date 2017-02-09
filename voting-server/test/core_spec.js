e67ed8355f5475ab23ce5d6272c276f1210273ce
Jérémy Lejeune <lejeune.jeremy@laposte.net>
2017-02-09 15:59:05 +0100

    Added tests for vote() function

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -1,7 +1,7 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries, next} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 describe('application logic', () => {
   describe('setEntries', () => {
@@ -36,4 +36,48 @@ describe('application logic', () => {
       }));
     });
   });
+  describe('vote', () => {
+    it('creates a tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Into the blue', 'Matrix')
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, 'Matrix');
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Into the blue', 'Matrix'),
+          tally: Map({
+            'Matrix': 1
+          })
+        }),
+        entries: List()
+      }));
+    });
+    it('adds to existing tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Matrix', 'Arrival'),
+          tally: Map({
+            'Matrix': 3,
+            'Arrival': 2
+          })
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, 'Arrival');
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Matrix', 'Arrival'),
+          tally: Map({
+            'Matrix': 3,
+            'Arrival': 3
+          })
+        }),
+        entries: List()
+      }));
+    });
+  });
+  // end of describe App logic
 });
