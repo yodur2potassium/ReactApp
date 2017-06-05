e8a539abeb8c56475312b10453d518e074fad85f
Jérémy Lejeune <lejeune.jeremy@laposte.net>
2017-06-05 16:39:57 +0200

    Finally managed to setup jsdom :)

--- voting-client/webpack.config.js
+++ voting-client/webpack.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  entry = [
+  entry: [
     './src/index.js'
   ],
+  module:{
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output : {
     path: __dirname + '/dist',
     publicPath: '/',
