562a5e10181a4450b9d479fbee463c4c74f11a31
Jérémy Lejeune <lejeune.jeremy@laposte.net>
2017-09-01 11:45:29 +0200

    Added tests on UI for disabled buttons and voted

--- voting-client/test/Voting_spec.jsx
+++ voting-client/test/Voting_spec.jsx
@@ -29,4 +29,22 @@ describe('Voting', () => {
     Simulate.click(buttons[0]);
     expect(votedWith).to.equal('Trainspotting');
   });
+  it('disables buttons when user has voted', () => {
+    const component = renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 days later"]}
+        hasVoted="Trainspotting" />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    expect(buttons.length).to.equal(2);
+    expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+    expect(buttons[1].hasAttribute('disabled')).to.equal(true);
+  });
+  it('adds label to the voted entry', () => {
+    const component = renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 days later"]}
+        hasVoted="Trainspotting" />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    expect(buttons[0].textContent).to.contain('Voted');
+  });
 });
